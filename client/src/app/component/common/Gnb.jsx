8b170e4c72683ccb2eb3d62036643003c9266e89
kay0829 <eluka97@daum.net>
2023-08-09 16:26:07 +0900

    [FE] 🔧 2) 예시 atom 추가 및 적용 예시

--- client/src/app/component/common/Gnb.jsx
+++ client/src/app/component/common/Gnb.jsx
@@ -1,11 +1,16 @@
+"use client";
+
 import Link from "next/link";
+import { useRecoilValue } from "recoil";
+import { userinfo } from "@/app/recoil/Global";
 
 function Gnb() {
+    const { nickname } = useRecoilValue(userinfo);
     return (
         <div>
             <Link href="/">Home</Link>
             <Link href="/questions">Questions</Link>
-            <Link href="/users">Users</Link>
+            <Link href="/users">{nickname}</Link>
         </div>
     );
 }
---
+++ client/src/app/recoil/Global.js
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const userinfo = atom({
+    key: "userinfo",
+    default: { nickname: "정열", email: "", profile: "" },
+});
