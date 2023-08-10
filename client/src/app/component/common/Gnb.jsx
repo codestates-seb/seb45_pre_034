257021a20460b22967621d052f04898f5b581c7a
kay0829 <eluka97@daum.net>
2023-08-10 09:33:20 +0900

    [FE] 🚚 2) jsx 파일 확장자 jsx로 명시

--- client/src/app/component/common/Gnb.jsx
+++ client/src/app/component/common/Gnb.jsx
@@ -7,10 +7,10 @@ import { userinfo } from "@/app/recoil/Global";
 function Gnb() {
     const { nickname } = useRecoilValue(userinfo);
     return (
-        <div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
             <Link href="/">Home</Link>
             <Link href="/questions">Questions</Link>
-            <Link href="/users">{nickname}</Link>
+            <Link href="/users">recoil value: {nickname}</Link>
         </div>
     );
 }
