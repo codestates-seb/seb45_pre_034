529490b6bda9aef9653617c193eed8595da898b9
kay0829 <eluka97@daum.net>
2023-08-11 14:22:06 +0900

    [FE] 🔧 43) 외부에서 이미지 가져오는 설정 추가 next.config.js (도메인 추가해줘야 함)

--- client/src/app/recoil/Global.js
+++ client/src/app/recoil/Global.js
@@ -2,5 +2,9 @@ import { atom } from "recoil";
 
 export const userinfo = atom({
     key: "userinfo",
-    default: { nickname: "정열", email: "", profile: "" },
+    default: {
+        nickname: "정열",
+        email: "",
+        profile: "https://item.kakaocdn.net/do/c5c470298d527ef65eb52883f0f186c49f17e489affba0627eb1eb39695f93dd",
+    },
 });
