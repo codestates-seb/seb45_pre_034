bf53837b93fb7d66665fe1d49f3757bbe21724aa
kay0829 <eluka97@daum.net>
2023-08-11 16:31:34 +0900

    [FE] 🔨 43) Layout 컴포넌트에 Navigation 컴포넌트 추가

--- client/src/app/component/layout/Layout.jsx
+++ client/src/app/component/layout/Layout.jsx
@@ -1,7 +1,15 @@
 import Header from "@component/layout/Header";
+import Navigation from "@component/layout/Navigation";
 
 function Layout() {
-    return <Header />;
+    return (
+        <>
+            <Header />
+            <div style={{ marginTop: "52px" }}>
+                <Navigation />
+            </div>
+        </>
+    );
 }
 
 export default Layout;
