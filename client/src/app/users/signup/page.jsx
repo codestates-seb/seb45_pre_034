56517296df12266dc038e8d50cbd85d07b716fe5
kay0829 <eluka97@daum.net>
2023-08-13 21:32:38 +0900

    [FE] 🔨 3) 회원가입 화면 page.js 컴포넌트 추가

--- client/src/app/users/signup/page.jsx
+++ client/src/app/users/signup/page.jsx
@@ -1,5 +1,29 @@
+import Header from "@component/layout/Header";
+import SignOauth from "@component/sign/SignOauth";
+import SignForm from "@component/sign/SignForm";
+import TailMsg from "@component/sign/TailMsg";
+
+import styles from "./SignUp.module.css";
+
 function SignUp() {
-    return <div>회원가입~</div>;
+    return (
+        <>
+            <Header />
+            <div style={{ marginTop: "52px" }} className={styles.container}>
+                <div className={styles.main_msg_container}>
+                    <h2 className={styles.main_msg}>Create your Stack Overflow account. It's free</h2>
+                    <h2 className={styles.main_msg}>And Only takes a minute.</h2>
+                </div>
+                <div className={styles.col_center}>
+                    <div className={styles.sign_form_container}>
+                        <SignOauth />
+                        <SignForm />
+                    </div>
+                    <TailMsg />
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default SignUp;
