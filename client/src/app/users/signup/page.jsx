b4a693715ba1a79a74588cf0f63b5f9e808e82d6
kay0829 <eluka97@daum.net>
2023-08-13 22:14:25 +0900

    [FE] 🔨 3) 회원가입 컴포넌트에 서비스 소개 문구 컴포넌트 추가

--- client/src/app/users/signup/page.jsx
+++ client/src/app/users/signup/page.jsx
@@ -1,4 +1,5 @@
 import Header from "@component/layout/Header";
+import JoinGuideMsg from "@component/sign/JoinGuideMsg";
 import SignOauth from "@component/sign/SignOauth";
 import SignForm from "@component/sign/SignForm";
 import TailMsg from "@component/sign/TailMsg";
@@ -10,11 +11,14 @@ function SignUp() {
         <>
             <Header />
             <div style={{ marginTop: "52px" }} className={styles.container}>
-                <div className={styles.main_msg_container}>
-                    <h2 className={styles.main_msg}>Create your Stack Overflow account. It's free</h2>
-                    <h2 className={styles.main_msg}>And Only takes a minute.</h2>
+                <div className={`${styles.col_center} ${styles.join_msg_container}`}>
+                    <JoinGuideMsg />
                 </div>
                 <div className={styles.col_center}>
+                    <div className={styles.main_msg_container}>
+                        <h2 className={styles.main_msg}>Create your Stack Overflow account. It's free</h2>
+                        <h2 className={styles.main_msg}>And Only takes a minute.</h2>
+                    </div>
                     <div className={styles.sign_form_container}>
                         <SignOauth />
                         <SignForm />
