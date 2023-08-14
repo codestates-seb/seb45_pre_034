f6f1fb95c61f6d1bdd4aa8fcbbe3eb65294a45cb
kay0829 <126226314+kay0829@users.noreply.github.com>
2023-08-14 10:55:38 +0900

    [FE] 💄 3) 회원가입폼 (#66)
    * [FE] 💄 3) ul, li padding, margin 없애기 위한 스타일 추가
    
    * [FE] 🍱 3) oauth 아이콘 이미지 추가
    
    * [FE] 🔨  3) oauth 버튼 컴포넌트 추가
    
    * [FE] 🔨 3) sign/Button.jsx 버튼 컴포넌트 리팩토링 및 전달받을 props 추가(isFullBtn: width가 100% 인지 / fit-content인지)
    
    * [FE] 🔨 3) sign/SignInput.jsx 회원가입/로그인에 사용할 input 컴포넌트 추가
    
    * [FE] 🔨 3) 회원가입 입력폼 컴포넌트 추가
    
    * [FE] 🔨 3) sign/TailMsg.jsx 회원가입/로그인에 하단 문구 컴포넌트 추가
    
    * [FE] 🔨 3) 회원가입 화면 page.js 컴포넌트 추가
    
    * [FE] 🐛 3) scroll이 생기지 않던 에러 수정 (body에 overflow: hidden 스타일 삭제)
    
    * [FE] 🐛 3) 스크롤 시 헤더 컴포넌트가 가려지지 않도록 수정
    
    * [FE] 🔨 3) 회원가입 입력폼 옆 서비스 소개 문구 컴포넌트 추가
    
    * [FE] 🔨 3) 회원가입 컴포넌트에 서비스 소개 문구 컴포넌트 추가
    
    * [FE] 💄3) 반응형 스타일링 관련 부자연스러운 부분 수정

---
+++ client/src/app/component/sign/SignForm.jsx
@@ -0,0 +1,28 @@
+import SignInput from "@component/sign/SignInput";
+import Button from "@component/sign/Button";
+import styles from "./SignForm.module.css";
+
+function SignForm() {
+    const nicknameSubGuide = `Display name can only
+    \n- contains letters, digits, spaces, hyphens or apostrophes
+    \n- start with a letter of digis
+    \n- be 3 to 30 characters long`;
+
+    const passwordSubGuide = `Passwords must contain at least eight characters, including at least 1 letter and 1 number.`;
+    return (
+        <div className={styles.container}>
+            <SignInput label="Display name(Nickname)" subGuideMsg={nicknameSubGuide} />
+            <SignInput label="Email" />
+            <SignInput label="Password" subGuideMsg={passwordSubGuide} />
+
+            <Button label="Sign up" type="Primary" isFullBtn={true} />
+
+            <p className={styles.guide_msg}>
+                By clicking “Sign up”, you agree to our terms of service and acknowledge that you have read and
+                understand our privacy policy and code of conduct.
+            </p>
+        </div>
+    );
+}
+
+export default SignForm;
