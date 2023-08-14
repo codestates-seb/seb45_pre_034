dc387fc9a5cb523f76d2dc02efe98e7cc2d47439
toa <gimhyoungsoo@gmail.com>
2023-08-14 16:05:34 +0900

    ✨ QuestionSummary 컴포넌트 생성 - .ul_tag의 li 요소는 Tag.jsx 컴포넌트로 대체 될 예정

---
+++ client/src/app/component/questions/QuestionSummary.jsx
@@ -0,0 +1,55 @@
+import styles from "@component/questions/QuestionSummary.module.css";
+
+function QuestionSummary() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.summary}>
+                <div className={styles.summary_stat}>
+                    <div>
+                        <span>0</span>
+                        <span>votes</span>
+                    </div>
+                    <div>
+                        <span>0</span>
+                        <span>answer</span>
+                    </div>
+                    <div>
+                        <span>0</span>
+                        <span>views</span>
+                    </div>
+                </div>
+                <div>
+                    <h3 className={styles.h3}>
+                        <a href="#">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, magni. Autem tempora magnam
+                            dicta! Ipsa dolores et architecto voluptas
+                        </a>
+                    </h3>
+
+                    <div className={styles.meta}>
+                        <ul className={styles.ul_tag}>
+                            <li>sql</li>
+                            <li>sql-server</li>
+                            <li>sql-like</li>
+                        </ul>
+
+                        <div className={styles.user_card}>
+                            <div>
+                                <img src="" alt="유저 아바타" />
+                            </div>
+                            <div>
+                                <a href="#">유저 닉네임</a>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    modified <span>10 minutes ago</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default QuestionSummary;
