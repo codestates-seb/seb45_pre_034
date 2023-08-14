33eeb5bd624b6e7b16e80f8190e97293b3aad01c
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-08-14 15:31:51 +0900

    :wrench: 답변 dto 생성

--- server/src/main/java/com/preproject/stackoverflow/answer/dto/AnswerPatchDto.java
+++ server/src/main/java/com/preproject/stackoverflow/answer/dto/AnswerPatchDto.java
@@ -1,9 +1,12 @@
 package com.preproject.stackoverflow.answer.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class AnswerPatchDto {
+    private Integer answerId;
     private Integer questionId;
     private Integer userId;
     private String body;
