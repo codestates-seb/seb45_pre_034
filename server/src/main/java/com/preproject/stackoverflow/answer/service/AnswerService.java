a99951d488ceb984784f1253c1659b80e8157e29
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-08-14 15:11:05 +0900

    :wrench: [BE] 72) CRUD 구현

--- server/src/main/java/com/preproject/stackoverflow/answer/service/AnswerService.java
+++ server/src/main/java/com/preproject/stackoverflow/answer/service/AnswerService.java
@@ -2,6 +2,8 @@ package com.preproject.stackoverflow.answer.service;
 
 import com.preproject.stackoverflow.answer.entity.Answer;
 import com.preproject.stackoverflow.answer.repository.AnswerRepository;
+import com.preproject.stackoverflow.exception.BusinessLogicException;
+import com.preproject.stackoverflow.exception.ExceptionCode;
 import com.preproject.stackoverflow.question.entity.Question;
 import org.springframework.stereotype.Service;
 
@@ -16,20 +18,75 @@ public class AnswerService {
         this.answerRepository = answerRepository;
     }
 
-//    public Answer createAnswer(Answer answer){
-//
-//    }
-
-    /** BUSINESS LOGIC EXCEPTION 클래스 생성 이전에 임시로 RuntimeException 사용 **/
-    public Answer findAnswer(int answerId){
-        Optional<Answer> optionalAnswer = answerRepository.findById(answerId);
-        Answer findAnswer = optionalAnswer.orElseThrow(() ->
-                new RuntimeException("404_NOT_FOUND"));
-        return findAnswer;
+    /**
+     * POST 메서드 사용시 필요한 create 메서드
+     * @param answer
+     * @return answer 생성
+     */
+    public Answer createAnswer(Answer answer){
+        return answerRepository.save(answer);
     }
-    /**임시로 runtime exception에 넣었습니다**/
+
+    /**
+     * PATCH 메서드 사용시 필요한 업데이트 메서드
+     * @param answer
+     * @return 수정된 answer
+     */
+    public Answer updateAnswer(Answer answer){
+        Answer findAnswer = findVerifiedAnswer(answer.getAnswerId());
+
+        Optional.ofNullable(answer.getBody())
+                .ifPresent(body -> findAnswer.setBody(body));
+        Optional.ofNullable(answer.getRecommend())
+                .ifPresent(recommend -> findAnswer.setRecommend(recommend));
+        findAnswer.setStatus(answer.isStatus());
+
+        return answerRepository.save(findAnswer);
+    }
+
+    /**
+     *
+     * @param answerId
+     * @return Answer 번호로 찾아진 Answer
+     */
+    public Answer findAnswer(int answerId){
+        return findVerifiedAnswer(answerId);
+    }
+
+    /**
+     *
+     * @param questionId
+     * @return QuestionId로 찾아진 전체 리스트
+     */
     public List<Answer> findQuestionAnswer(int questionId){
         List<Answer> answerList = answerRepository.findAllByQuestionId(questionId);
         return answerList;
     }
+
+    /**
+     *
+     * @param userId
+     * @return UserId로 찾아진 전체 리스트
+     */
+    public List<Answer> findUserAnswer(int userId){
+        List<Answer> answerList = answerRepository.findAllByUserId(userId);
+        return answerList;
+    }
+
+    /**
+     * 답변 삭제
+     * @param answerId
+     */
+    public void deleteAnswer(int answerId){
+        answerRepository.deleteById(answerId);
+    }
+    public Answer findVerifiedAnswer(int answerId){
+        Optional<Answer> optionalAnswer = answerRepository.findById(answerId);
+        Answer findAnswer =
+                optionalAnswer.orElseThrow(()
+                        -> new BusinessLogicException(ExceptionCode.ANSWER_NOT_FOUND));
+
+        return findAnswer;
+    }
+
 }
