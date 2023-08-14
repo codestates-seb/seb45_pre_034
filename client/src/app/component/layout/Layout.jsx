bf5f558b53929b96fba4290a58c4c7620ae87c3f
kay0829 <eluka97@daum.net>
2023-08-14 12:37:03 +0900

    [FE] 🔨 24) Layout 컴포넌트에서 Navigation 탭 계정 유무에 따라 보이거나 보이지 않도록 수정

--- client/src/app/component/layout/Layout.jsx
+++ client/src/app/component/layout/Layout.jsx
@@ -1,15 +1,27 @@
+"use client";
+
+import { useRecoilValue } from "recoil";
+import { userinfo } from "@recoil/Global";
+
 import Header from "@component/layout/Header";
 import Navigation from "@component/layout/Navigation";
-import QuestionsList from "@component/questions/QuestionsList";
 import styles from "@component/layout/Layout.module.css";
 
 function Layout({ children }) {
+    const { email } = useRecoilValue(userinfo);
+
+    const renderNavigationByAuthAndWidth = () => {
+        if (email) {
+            return <Navigation />;
+        }
+    };
+
     return (
         <>
             <Header />
             <div className={styles.under_header_container}>
-                <Navigation />
-                { children }
+                {renderNavigationByAuthAndWidth()}
+                {children}
             </div>
         </>
     );
