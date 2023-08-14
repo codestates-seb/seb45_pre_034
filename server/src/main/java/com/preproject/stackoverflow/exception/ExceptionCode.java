b67f4a45e1af84dbbdc868bd300b17f12b259375
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-08-14 09:40:53 +0900

    :recycle: Answer HTTP 에러 변수 추가

--- server/src/main/java/com/preproject/stackoverflow/exception/ExceptionCode.java
+++ server/src/main/java/com/preproject/stackoverflow/exception/ExceptionCode.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 public enum ExceptionCode {
     QUESTION_NOT_FOUND(404, "Question not found"),
     QUESTION_EXISTS(409, "Question exists"),
-    NOT_IMPLEMENTATION(501, "Not Implementation");
+    NOT_IMPLEMENTATION(501, "Not Implementation"),
+    ANSWER_NOT_FOUND(404, "Answer not found"),
+    ANSWER_EXISTS(409, "Answer exists");
 
 
     @Getter
