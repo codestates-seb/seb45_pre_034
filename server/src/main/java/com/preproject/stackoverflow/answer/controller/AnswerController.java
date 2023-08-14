aa45abb60400f352c349c56e400105840fd6b389
prinzusseugen <74959653+prinzusseugen@users.noreply.github.com>
2023-08-14 15:50:34 +0900

    :wrench: 컨트롤러, repository 생성

--- server/src/main/java/com/preproject/stackoverflow/answer/controller/AnswerController.java
+++ server/src/main/java/com/preproject/stackoverflow/answer/controller/AnswerController.java
@@ -1,5 +1,7 @@
 package com.preproject.stackoverflow.answer.controller;
 
+import com.preproject.stackoverflow.answer.dto.AnswerPatchDto;
+import com.preproject.stackoverflow.answer.dto.AnswerPostDto;
 import com.preproject.stackoverflow.answer.entity.Answer;
 import com.preproject.stackoverflow.answer.mapper.AnswerMapper;
 import com.preproject.stackoverflow.answer.service.AnswerService;
@@ -7,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import java.util.List;
 
@@ -21,17 +24,40 @@ public class AnswerController {
         this.answerMapper = answerMapper;
     }
 
-//    @PostMapping
-//    public ResponseEntity postAnswer(@RequestParam("questionId") int questionId){
-//        Answer answer = AnswerService.
-//    }
+    /**
+     * 답변 작성
+     * @param answerPostDto
+     * @return 201_CREATED
+     */
+    @PostMapping
+    public ResponseEntity postAnswer(@Valid @RequestBody AnswerPostDto answerPostDto){
+        Answer answer = answerService.createAnswer(answerMapper.answerPostDtoToAnswer(answerPostDto));
+
+        return new ResponseEntity<>(answerMapper.answerToAnswerResponseDto(answer), HttpStatus.CREATED);
+    }
 
     /**
-     * 특정 질문에 대한 전체 답변 출력 메서드
-     * @param questionId 질문 id
-     * @author wongwan choi
+     * 답변 수정
+     * @param answerId
+     * @param answerPatchDto
+     * @return 200_OK
      */
-    @GetMapping("/{questionId}")
+    @PatchMapping("/{answerId}")
+    public ResponseEntity patchAnswer(@PathVariable("answerId") @Positive int answerId,
+                                      @Valid @RequestBody AnswerPatchDto answerPatchDto){
+        answerPatchDto.setAnswerId(answerId);
+
+        Answer answer = answerService.updateAnswer(answerMapper.answerPatchDtoToAnswer(answerPatchDto));
+
+        return new ResponseEntity<>(answerMapper.answerToAnswerResponseDto(answer), HttpStatus.OK);
+    }
+
+    /**
+     * 특정 질문에 대한 전체 답변
+     * @param questionId 질문 id
+     * @return 200_OK, 질문에 대한 전체 답변
+     */
+    @GetMapping("/question/{questionId}")
     public ResponseEntity getQuestionAnswer(@PathVariable("questionId") @Positive int questionId){
         List<Answer> answers = answerService.findQuestionAnswer(questionId);
         return new ResponseEntity<>(
@@ -39,11 +65,11 @@ public class AnswerController {
                 HttpStatus.OK);
     }
     /**
-     * 특정 유저에 대한 전체 답변 출력 메서드
+     * 특정 유저에 대한 전체 답변
      * @param userId
-     * @author wongwan choi
+     * @return 200_OK, 유저에 대한 전체 답변
      * */
-    @GetMapping("/{userId}")
+    @GetMapping("/user/{userId}")
     public ResponseEntity getUserAnswer(@PathVariable("userId") @Positive int userId){
         List<Answer> answers = answerService.findUserAnswer(userId);
         return new ResponseEntity<>(
@@ -52,6 +78,16 @@ public class AnswerController {
         );
     }
 
+    /**
+     * 해당 답변 삭제
+     * @param answerId
+     * @return 204_NO_CONTENT
+     */
+    @DeleteMapping("/{answerId}")
+    public ResponseEntity deleteAnswer(@PathVariable("answerId") @Positive int answerId){
+        answerService.deleteAnswer(answerId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 
 }
