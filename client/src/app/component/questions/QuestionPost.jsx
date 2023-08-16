2f62d1c40cf9f1828685a271ce956a8e9ffa3de7
toa <gimhyoungsoo@gmail.com>
2023-08-16 18:48:29 +0900

    💄 질문 컴포넌트 협의 후 변경

--- client/src/app/component/questions/QuestionPost.jsx
+++ client/src/app/component/questions/QuestionPost.jsx
@@ -29,29 +29,28 @@ function QuestionPost() {
                 <div className={styles.post_grid}>
                     <div className={styles.vote_cell}>voiting container</div>
                     <div className={styles.post_cell}>
-                        <div>본문 컴포넌트</div>
+                        <div>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quia voluptates eos
+                            unde exercitationem obcaecati, quasi tenetur magni veniam. Qui assumenda, facere quis
+                            veritatis omnis hic similique aliquam rerum laudantium? Lorem ipsum, dolor sit amet
+                            consectetur adipisicing elit. Non voluptatibus sint doloribus. Commodi corporis odit quasi
+                            officia quos dolorem expedita soluta necessitatibus, atque, culpa placeat, perspiciatis
+                            minus suscipit fuga! Nisi.
+                        </div>
                         <div className={styles.post_cell_bottom}>
-                            <div>menu</div>
+                            <div>
+                                <div>
+                                    <span>share</span>
+                                    <span>edit</span>
+                                    <span>follow</span>
+                                </div>
+                            </div>
                             <div>signiture</div>
                             <div>signiture owner</div>
                         </div>
                     </div>
                     <div>{/* grid의 cell을 채우는 용도의 요소 */}</div>
-                    <div>
-                        {/* 질문글과 답변에 comment가 가능하므로 컴포넌트화 해야함 */}
-                        comment 컴포넌트 필요
-                        <div>post comments area</div>
-                        <div>
-                            <a href="#">Add a comment</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div>답변 목록</div>
-
-                <div>
-                    Your Answer
-                    <Mde />
+                    <div>{/* Comment.jsx  */}add comment</div>
                 </div>
             </section>
         </article>
