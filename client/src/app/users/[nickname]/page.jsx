03bfe6336afd0634b135ea1e5ebe4cfca2d2f816
kay0829 <eluka97@daum.net>
2023-08-16 15:33:52 +0900

    [FE] 💄 80) 유저 상세 페이지 추가

--- client/src/app/users/[nickname]/page.jsx
+++ client/src/app/users/[nickname]/page.jsx
@@ -1,7 +1,44 @@
+import Layout from "@component/layout/Layout";
+import UserCard from "@component/user/UserCard";
+import IconDefaultUser from "/public/icon_default_user.png";
+
+import styles from "./UserDetail.module.css";
+
+import { user, questions, answers, comments } from "./mockdata";
+import ActivityItem from "@component/user/ActivityItem";
+
 function UserDetail({ params }) {
     const { nickname } = params;
 
-    return <div>{nickname}님 유저 상세페이지</div>;
+    return (
+        <Layout>
+            <div className={styles.container}>
+                <div className={styles.user_card_container}>
+                    <UserCard user={user} />
+                </div>
+                <div>
+                    <h2 className={styles.sub_title}>Questions</h2>
+                    <div className={styles.sub_list}>
+                        {questions.map((v) => (
+                            <ActivityItem question={v} />
+                        ))}
+                    </div>
+                    <h2 className={styles.sub_title}>Answers</h2>
+                    <div className={styles.sub_list}>
+                        {answers.map((v) => (
+                            <ActivityItem answer={v} />
+                        ))}
+                    </div>
+                    <h2 className={styles.sub_title}>Comments</h2>
+                    <div className={styles.sub_list}>
+                        {comments.map((v) => (
+                            <ActivityItem comment={v} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
 }
 
 export default UserDetail;
