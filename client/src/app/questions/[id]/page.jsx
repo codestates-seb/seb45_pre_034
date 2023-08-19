1e07f04d94cc31dbdf7a0077b11ef1c6e72329d2
toa <gimhyoungsoo@gmail.com>
2023-08-19 14:36:57 +0900

    ♻️  MdViewer가 QuestionPost 내부에 위치하도록 변경

--- client/src/app/questions/[id]/page.jsx
+++ client/src/app/questions/[id]/page.jsx
@@ -5,34 +5,11 @@ import Mde from "@component/common/Mde";
 import MdViewer from "@component/common/MdViewer";
 import QuestionPost from "@component/questions/QuestionPost";
 function Questions() {
-    
-    const str = `
-### Preview Markdown
-
-[![Open in CodeSandbox](https://img.shields.io/badge/Open%20in-CodeSandbox-blue?logo=codesandbox)](https://codesandbox.io/embed/react-md-editor-preview-markdown-vrucl?fontsize=14&hidenavigation=1&theme=dark)
-
-\`\`\`jsx
-import React from "react";
-import ReactDOM from "react-dom";
-import MDEditor from '@uiw/react-md-editor';
-
-export default function App() {
-  return (
-    <div className="container">
-      <MDEditor.Markdown source="Hello Markdown!" />
-    </div>
-  );
-}
-\`\`\`
-> 이렇게 작성한 코드가 어쩌구 저쩌구......
-<br/>
-`;
-
+ 
     return (
         <>
             <Layout>
                 <QuestionPost />
-                <MdViewer source={str}/>
                 <Mde />
             </Layout>
         </>
