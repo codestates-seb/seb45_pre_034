3e5960208bdacff6e47f5b5d31685f348f693d46
toa <gimhyoungsoo@gmail.com>
2023-08-20 17:47:23 +0900

    🐛 붙여넣기와 최대 텍스트 수 음수 출력 관련 버그 해결

--- client/src/app/component/comment/Comment.jsx
+++ client/src/app/component/comment/Comment.jsx
@@ -1,46 +1,63 @@
 "use client";
-
-import { useRef, useState } from "react";
-import styles from "@component/comment/Comment.module.css";
 import Button from "@component/sign/Button";
+import { useEffect, useRef, useState } from "react";
+import styles from "@component/comment/Comment.module.css";
+const MIN_LENGTH = 15;
+const MAX_LENGTH = 500;
+
 function Comment() {
-    const LIMIT_OF_COMMENT = 500;
     const inputRef = useRef(null);
     const [isFormOpen, setIsFormOpen] = useState(false);
-    const [textLength, setTextLength] = useState(0);
-    const [labelText, setLabelText] = useState("Enter at least 15 characters");
-
-    const AddCommentHandler = () => {
+    const [inputText, setInputText] = useState("");
+    const [labelText, setLabelText] = useState("");
+    const OpenFormHandler = () => {
         setIsFormOpen(!isFormOpen);
     };
 
-    const handleInputChange = () => {
-        const inputValue = inputRef.current.value;
-        setTextLength(inputValue.length);
-
-        if (textLength >= 15) {
-            setLabelText(`${LIMIT_OF_COMMENT - textLength} characters left`);
-        } else {
-            setLabelText("Enter at least 15 characters");
-        }
+    const InputChangeHandler = () => {
+        setInputText(inputRef.current.value);
     };
+
+    const SetNoticeHandler = () => {
+        let notice_string;
+
+        if (inputText.length >= MAX_LENGTH) {
+            //최대 이상일 때
+            notice_string = "You can no longer create it";
+        } else {
+            if (inputText.length >= MIN_LENGTH) {
+                notice_string = `${MAX_LENGTH - inputText.length} characters left`;
+            } else {
+                //최소 미만일 때
+                notice_string = "Enter at least 15 characters";
+            }
+        }
+        setLabelText(notice_string);
+    };
+
+    useEffect(() => {
+        SetNoticeHandler();
+    }, [inputText]);
 
     return (
         <>
             {/* 댓글 데이터가 있다면 댓글 목록을 표시함  */}
             {isFormOpen ? (
-                <div >
+                <div>
                     <form action="#" className={`${styles.flex}`}>
                         <div>
                             <textarea
-                                id="comment_input"
-                                className={`${styles.block} ${styles.input}` }
-                                ref={inputRef}
-                                onChange={handleInputChange}
+                                id="comment"
+                                className={`${styles.block} ${styles.input}`}
                                 type="text"
+                                maxlength={MAX_LENGTH}
                                 placeholder="Use comments to reply to other users or notify them of changes. if you are adding new information, edit your post instead of commenting."
+                                ref={inputRef}
+                                // value={inputText}
+                                onChange={InputChangeHandler}
+                                onPaste={InputChangeHandler}
                             />
-                            <label htmlFor="comment_input" className={styles.block}>
+                            <label htmlFor="comment" className={styles.block}>
                                 <span>{labelText}</span>
                             </label>
                         </div>
@@ -48,7 +65,7 @@ function Comment() {
                     </form>
                 </div>
             ) : (
-                <div onClick={AddCommentHandler}>{"Add a comment"}</div>
+                <div onClick={OpenFormHandler}>{"Add a comment"}</div>
             )}
         </>
     );
