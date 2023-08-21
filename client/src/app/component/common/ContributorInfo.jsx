e03aa4f4cbc863d1809312874fe8857410dd248f
toa <gimhyoungsoo@gmail.com>
2023-08-21 23:11:05 +0900

    ✨💄 ContributorInfo 컴포넌트 추가

---
+++ client/src/app/component/common/ContributorInfo.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import styles from "@component/common/Contributor.module.css";
+function ContributorInfo() {
+    const UserInfo = () => {
+        return (
+            <div className={styles.user_info}>
+                <div>user action time</div>
+                <div>user details</div>
+            </div>
+        );
+    };
+
+    return (
+        <>
+            <div className={styles.container}>
+                <div className={styles.actions}>
+                    <div>
+                        <a href="#">share</a>
+                    </div>
+                    <div>
+                        <a href="#">edit</a>
+                    </div>
+                    <div>
+                        <a href="#">follow</a>
+                    </div>
+                </div>
+                <div className={styles.signature}>{UserInfo()}</div>
+                <div className={styles.signature_owner}>{UserInfo()}</div>
+            </div>
+        </>
+    );
+}
+export default ContributorInfo;
