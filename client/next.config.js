963112a5d59cd9375a31e125951838a6bb54dfd2
kay0829 <eluka97@daum.net>
2023-08-22 14:48:47 +0900

    [FE] 🔧 26) 외부 도메인 이미지 hostname next.js 설정에 추가

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "passionfull.s3.ap-northeast-2.amazonaws.com",
+                port: "",
+                pathname: "/images/**",
+            },
+            {
+                protocol: "https",
+                hostname: "robohash.org",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
     productionBrowserSourceMaps: true,
 };
 const removeImports = require("next-remove-imports")();
